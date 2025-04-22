10bf719a3938152ac4bf41cb9dff5827826495f8
PVL06 <pverl06@gmail.com>
2025-04-22 10:40:34 +0200

    Django 3 -> 3.2, update requirements

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -110,6 +110,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'oc_lettings_site.wsgi.application'
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
